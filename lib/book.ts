99cb4c0471cfbe73ab6da1582b85c30f608f6df7
davidps79 <1006352045@u.icesi.edu.co>
2024-05-25 19:49:30 -0500

    Merge

--- lib/book.ts
+++ lib/book.ts
@@ -1,3 +1,19 @@
+import { Author } from "./author";
+
+export type Book = {
+  id: string;
+  title: string;
+  publisher: string;
+  author: Author;
+  overview: string;
+  price: number;
+  stock: number;
+  isbn: string;
+  rating: number;
+  category: string;
+  ebookCover: string;
+}
+
 import { InfoEbookDto } from "./ebook";
 
 export class EbookCartItem {
