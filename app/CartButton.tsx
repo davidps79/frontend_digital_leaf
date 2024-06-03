44fb10343184fddbb2203815e0c13f7b8c88f9ae
davidps79 <1006352045@u.icesi.edu.co>
2024-06-03 01:42:11 -0500

    Minor change

--- app/CartButton.tsx
+++ app/CartButton.tsx
@@ -1,33 +1,35 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { Button } from '@/components/ui/button';
 import { updateCart, openCart } from '@/redux/cartSlice';
 import { InfoEbookDto } from '@/lib/ebook';
 import { IconShoppingBagPlus } from '@tabler/icons-react';
 import LoginRequiredPopup from './LoginRequiredPopup';
-import { AlertDialogContext } from './AlertDialogProvider';
 
 const CartButton = ({ ebook }: { ebook: InfoEbookDto }) => {
     const dispatch = useAppDispatch();
+    const [showLoginPopup, setShowLoginPopup] = useState(false);
     const token = useAppSelector((state) => state.auth.token);
-    const { showAlertDialog } = useContext(AlertDialogContext);
-
+    
     const handleAddToCart = () => {
         if (!token) {
-            showAlertDialog("Inicia sesión", "Debes iniciar sesión para comprar un libro");
+            setShowLoginPopup(true);
         } else {
             dispatch(updateCart({ ebookIds: [ebook.id], operation: 'add' }));
             dispatch(openCart());
         }
-
+        
     };
 
     return (
         <div className='w-full'>
-            <Button onClick={handleAddToCart} variant="default" size="lg" className='w-full'>
-                Añadir al carrito
-                <IconShoppingBagPlus className='ml-2' />
-            </Button>
+        <Button onClick={handleAddToCart} variant="default" size="lg" className='w-full'>
+            Añadir al carrito
+            <IconShoppingBagPlus className='ml-2' />
+        </Button>
+            {showLoginPopup && (
+                <LoginRequiredPopup onClose={() => setShowLoginPopup(false)} />
+            )}
         </div>
     );
 };
