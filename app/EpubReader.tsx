369ae03e24166c99e2093b6a047e0116e3fbb32b
davidps79 <1006352045@u.icesi.edu.co>
2024-06-02 21:36:52 -0500

    Update reader

--- app/EpubReader.tsx
+++ app/EpubReader.tsx
@@ -8,33 +8,41 @@ import { useAppSelector } from '@/redux/hooks';
 import { notFound } from 'next/navigation';
 import logo from '@/public/logo.png'
 import Image from 'next/image';
+import { InfoEbookDto } from '@/lib/ebook';
+import Link from 'next/link';
 
 const EpubReader = ({ ebookId }: { ebookId: string }) => {
-    const [location, setLocation] = useState<number>(1);
+    const [location, setLocation] = useState<string>("0");
     const [error, setError] = useState(false);
     const [ebook, setEbook] = useState<{ buffer: ArrayBuffer, title: string, ownsBook: boolean } | null>(null);
     const token = useAppSelector((state) => state.auth.token);
     const userId = useAppSelector((state) => state.auth.user?.id) || (typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('user') || '{}').id : null);
+    const [visited, setVisited] = useState(new Set());
 
     useEffect(() => {
         const fetchEbook = async () => {
-            const ebook = await getBooksInfo(ebookId);
-            const data = await downloadEbook(ebookId);
-
-            if (!ebook || !data) {
+            const ebook: InfoEbookDto = await getBooksInfo(ebookId);
+            if (!ebook) {
                 setError(true)
                 return;
             }
+
+            const data = await downloadEbook(ebook.fileUrl);
+            if (!data) {
+                setError(true)
+                return;
+            }
+
+            const buffer = await data.arrayBuffer();
 
             let ownsBook = false;
             if (token) {
                 const bought = await checkBookOwnership(userId, ebook.id);
                 const isAuthor = await checkBookAuthor(userId, ebook.id);
                 ownsBook = bought || isAuthor;
-
-                const buffer = await data.arrayBuffer();
-                setEbook({ buffer, ownsBook, title: ebook.title });
             }
+
+            setEbook({ buffer, ownsBook, title: ebook.title });
         }
 
         fetchEbook();
@@ -54,16 +62,36 @@ const EpubReader = ({ ebookId }: { ebookId: string }) => {
     )
 
     return (
-        <ReactReader
-            url={ebook.buffer}
-            title={ebook.title}
-            location={location}
-            locationChanged={(epubcfi: string) => {
-                const newValue = Number(epubcfi);
-                if (ebook.ownsBook || newValue < 6)
-                    setLocation(newValue)
-            }}
-        />
+        (ebook.ownsBook || visited.size < 5) ?
+            <ReactReader
+                url={ebook.buffer}
+                title={ebook.title}
+                location={location}
+                locationChanged={(epubcfi: string) => {
+                    setLocation(epubcfi)
+
+                    console.log(epubcfi)
+                    const newV = new Set(visited)
+                    newV.add(epubcfi)
+                    setVisited(newV)
+                }}
+            />
+            :
+            <div className='w-full h-[80vh] flex justify-center items-center'>
+                <div className='flex flex-col gap-2'>
+                    <span className='text-lg font-semibold'>
+                        Has llegado al fin de la vista previa
+                    </span>
+                    <span className='mb-6'>
+                        Compra el libro y accede al contenido completo
+                    </span>
+
+                    <Link className='underline underline-offset-1 hover:font-semibold transition-all' href={"/ebook/" + ebookId}>
+                        Regresar a la tienda
+                    </Link>
+                </div>
+            </div>
+
     )
 }
 
